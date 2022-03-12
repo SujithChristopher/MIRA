57e33168942313d78380a1a0cb2d100302308707
Sujith Christopher <50609700+SujithChristopher@users.noreply.github.com>
2022-03-12 12:25:25 +0530

    testing

--- nano_imu/classic_imu_py/imu_test.py
+++ nano_imu/classic_imu_py/imu_test.py
@@ -1,6 +1,5 @@
 """This program is for recording IMU data, through HC05 bluetooth module"""
 
-from itertools import starmap
 import serial
 import struct
 import keyboard
@@ -28,7 +27,7 @@ class SerialPort(object):
         if csv_enable:
             self.csv_file = open(csv_path + "//imu01.csv", "w")
             self.csv = csv.writer(self.csv_file)
-            self.csv.writerow(["sys_time", "gx", "gy", "gz", "ax", "ay", "az"])
+            self.csv.writerow(["sys_time", "ax", "ay", "az", "gx", "gy", "gz"])
         self.triggered = True
         self.connected = False
         
@@ -52,8 +51,10 @@ class SerialPort(object):
 
     def serial_read(self):
         """returns bool for valid read, also returns the data read"""
+        # print(self.ser_port.read())
        
         if (self.ser_port.read() == b'\xff') and (self.ser_port.read() == b'\xff'):
+            
             self.connected = True
             
             chksum = 255 + 255
@@ -64,47 +65,31 @@ class SerialPort(object):
             chksum += sum(self.payload)
             chksum = bytes([chksum % 256])
             _chksum = self.ser_port.read()
+            # print("inside", self.ser_port.read())
 
             return _chksum == chksum
+        
         return False
     
     def disconnect(self):
         stdout.write("disconnected\n")
-    
+        
     
     def run_program(self):
-        start = time.time()
-        while 1:
-            time.sleep(0.005)
+        for i in range(500):
+            
             if self.serial_read():
-                curr = time.time()
-                
                 val = struct.unpack("6f", self.payload)
                 nw = None
                 
                 if not nw:
                     nw = datetime.now()
+                    print(nw)
                 if self.csv_enabled:    
                     self.csv.writerow([str(nw), val[0], val[1], val[2], val[3], val[4], val[5]])
                 if keyboard.is_pressed("e"):
                     self.csv_file.close()
                     break
-                diff = curr - start
-                print(diff, datetime.now(), val[0])
-                start = curr
-            # if self.triggered and not self.connected:
-            #     self.disconnect()
-            #     break
-                _str = ("Accl: " +
-                        f"{val[0]:+1.3f}, " + 
-                        f"{val[1]:+1.3f}, " + 
-                        f"{val[2]:+1.3f} | " +
-                        "Gyro: " +
-                        f"{val[3]:+3.3f}, " +
-                        f"{val[4]:+3.3f}, " +
-                        f"{val[5]:+3.3f}")
-                # stdout.write(_str)
-                # stdout.flush()
             
     
 if __name__ == '__main__':
