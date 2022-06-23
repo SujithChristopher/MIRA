6433bb826840b10c10402400efe08c9164205687
Sujith Christopher <chrisbon95@gmail.com>
2022-06-23 16:05:53 +0530

    rewriting the code for better robustness and readability

---
+++ support_py/parameter_init.py
@@ -0,0 +1,36 @@
+from PyQt5.QtGui import *
+
+
+def initialize_parameters(self):
+    self.yRes = 736
+    self.xRes = 864
+
+    self.xPos = 0
+    self.yPos = 0
+
+    self.fps_val = 15
+
+    self.setupUi(self)
+    self.poseFlag = "RECT"
+    self.dataFlag = 0
+
+    self.newSes = False
+    self.capF = False
+    self.newSesBr = False
+    self.calib = False
+
+    self.profilePicture = None
+
+    self.calibCounter = 0
+    self.calibXYZ = []
+    self.calibXYZrs = []
+
+    self.globDate = self.dateEdit.date()
+    self.statusBar().showMessage("Initialized")
+    self.statusBar().setFont(QFont('Times', 12))
+
+    """
+    Fixed roi parameters
+    """
+    self.fx_roi = False
+    self.imu_trigger = True
