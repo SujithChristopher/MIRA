e60b85b94fa8680c03bec7681f03c3a8c834db05
Sujith Christopher <chrisbon95@gmail.com>
2022-07-19 10:13:15 +0530

    organizing

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from PyQt5.QtWidgets import *
 
 """this is the main design file"""
 from guiDesign import Ui_MainWindow
+from function_class import Mira_functions # putting all my main functions here, to organize the code
 
 """importing pykinect modules"""
 from pykinect2 import PyKinectV2
@@ -181,7 +182,7 @@ class Worker(QRunnable):
 """Main program"""
 
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class MainWindow(Mira_functions):
     xyRectPos = pyqtSignal(int)
     imageStatus = pyqtSignal(str)
     progress_callback = pyqtSignal(QImage)
@@ -246,162 +247,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.refreshICN.setIcon(QIcon(r'src\refreshIcon.png'))
         self.keyPressEvent = self.keyboard_events
 
-        
-
-    def keyboard_events(self, event):
-        if event.key() == Qt.Key_Escape:
-            self.uns.setChecked(True)
-
-    def closeEvent(self, event):
-        if (not self.data_saved) and (self.dummy_dir is not ""):
-            close = QtWidgets.QMessageBox.question(self,
-                                                   "QUIT",
-                                                   "Do you want to save the current session",
-                                                   QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
-
-            if close == QtWidgets.QMessageBox.Yes:
-                self.saveSesFun()
-                event.accept()
-            if close == QtWidgets.QMessageBox.No:
-                self.discardSesFun()
-                event.accept()
-            print("closing")
-            self.conn.close()
-            self.sconn.close()
-
-    def camera_list_clicked(self):
-        
-        inx = self.camera_list.currentRow()
-        _selected = self.device_list[inx]
-
-        if _selected.startswith("Kinect"):
-            self.selected_camera = "KINECT"
-
-        elif _selected.startswith("Intel"):
-            self.selected_camera = "INTEL"
-
-        else:
-            self.selected_camera = "WEBCAM"
-
-        print(self.selected_camera)
-
-    def fix_roifun(self):
-        if self.fix_roi.isChecked():
-            self.fx_roi = True
-        else:
-            self.fx_roi = False
-
-    def res_sett(self):
-        if self.hi_res_set.isChecked():
-            self.res_s = True
-        else:
-            self.res_s = False
-
-    def list_clicked(self):
-
-        row = self.p_list_wid.currentRow()
-        db_p_select(self, row)
-
-    def ses_clicked(self):
-        row = self.p_ses_wid.currentRow()
-        ses_name = self.ses_list[row]
-        p_uid = self.selected_p
-        n_pth = f"{self.data_path}//splitVideos//{p_uid}//{ses_name}//Video.avi"
-        self.openFile(n_pth)
-
-    def path_browse_fun(self):
-
-        self.temp_path = QFileDialog.getExistingDirectory(None, 'Select a data folder:', QDir.homePath())
-        self.statusBar().showMessage("Data path selected")
-
-    def sett_save_fun(self):
-
-        if self.temp_path == "":
-            self.statusbar.showMessage("Choose a directory")
-        else:
-            pth = f".//src//settings.pickle"
-            if os.path.exists(pth):
-                sett_file = open(pth, "wb")
-                pickle.dump(self.temp_path, sett_file)
-                sett_file.close()
-                try:
-                    if os.path.exists(".//src//settings.pickle"):
-                        sett_file = open(".//src//settings.pickle", "rb")
-                        sett = pickle.load(sett_file)
-                        sett_file.close()
-                        print(f"path: {sett}")
-                    else:
-                        print("Settings file not found, ending program")
-                        sys.exit()
-                except:
-                    print("Settings file corrupted")
-                    sys.exit()
-            else:
-                self.statusBar().showMessage("Something went wrong")
-            QMessageBox
-            msgBox = QMessageBox()
-            msgBox.setIcon(QMessageBox.Information)
-            msgBox.setText("Restart application to refresh data path")
-            msgBox.setWindowTitle("Restart")
-            msgBox.setStandardButtons(QMessageBox.Ok)
-            msgBox.buttonClicked.connect(self.exit_program)
-
-            returnValue = msgBox.exec()
-            if returnValue == QMessageBox.Ok:
-                print('OK clicked')
-
-    def exit_program(self):
-        sys.exit()
-
-    """ video playing functions """
-
-    def openFile(self, fileName):
-        # fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie",
-        #                                           QDir.homePath())
-        print(fileName)
-        if fileName != '':
-            self.mediaPlayer.setMedia(
-                QMediaContent(QUrl.fromLocalFile(fileName)))
-            self.vid_play.setEnabled(True)
-            # print("success")
-
-        db_fetch(self)
-        # print("success2")
-
-    def exitCall(self):
-        sys.exit(app.exec_())
-
-    def play(self):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.mediaPlayer.pause()
-            # pass
-        else:
-            self.mediaPlayer.play()
-
-    def mediaStateChanged(self, state):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.vid_play.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaPause))
-        else:
-            self.vid_play.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaPlay))
-
-    def positionChanged(self, position):
-        self.vid_slider.setValue(position)
-
-    def durationChanged(self, duration):
-        self.vid_slider.setRange(0, duration)
-
-    def setPosition(self, position):
-        self.mediaPlayer.setPosition(position)
-
-    def handleError(self):
-        self.vid_play.setEnabled(False)
-        # print("error")
-        # self.vid_player.setText("Error: " + self.mediaPlayer.errorString())
-        self.statusBar().showMessage("Error: " + self.mediaPlayer.errorString())
-
-    
+            
     """IMU function, for different purposes, this uses external trigger button"""
 
     def initializeIMU(self):
@@ -440,53 +286,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self._imu_p.terminate()
             print("imu recording stopped")
 
-    def select_task(self, event):
-        print(get_clickedtask(self))
-
-    def resetXYZFun(self):
-        self.calibCounter = 0
-        self.calibXYZ = []
-        self.selectXYZ.setText("Select 'O'")
-        self.acquireSingleframe()
-
-    def select_fun(self):
-
-        self.calibCounter = self.calibCounter + 1
-        self.calibXYZ.append((self.xPos, self.yPos))  # Store 1st position
-        self.calibXYZrs.append((self.xPos * 2, self.yPos * 2))
-        if self.calibCounter == 1:
-            self.selectXYZ.setText("Select X")
-        elif self.calibCounter == 2:
-            self.selectXYZ.setText("Select Y")
-        elif self.calibCounter == 3:
-            self.selectXYZ.setText("Show Calib")
-            self.calibDepth = self.kinectDepth.get_last_depth_frame()
-            self.calibPlot = True
-            self.calCalibData()
-        elif self.calibCounter == 4:
-            self.selectXYZ.setText("Save Calib")
-            self.saveCalib()
-
-    def saveCalib(self):
-        if self.calibPlot:
-            self.calib = False
-            print("Calibration will be saved during recording")
-        else:
-            print("No Calibration values found")
-
-    def calCalibData(self):
-        get_calCalibData(self)
-
-    def calibrateFun(self):
-        self.calibrate.hide()
-        self.exFun.hide()
-        self.calib = True
-        self.acquireSingleframe()
-
-    def capProfile(self):
-        self.capF = True
-        self.acquireSingleframe()
-        self.capF = False
+    
 
     def discardSesFun(self):
 
@@ -782,7 +582,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.fileCounter = 1
 
         self.createFile(self.fileCounter)
-
         p_packed = mp.packb(self.xy)
         self.paramFile.write(p_packed)
         p_packed = mp.packb(2)
