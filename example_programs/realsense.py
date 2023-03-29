4bf2ba79888062de6ee0edc6da99d7fa8aa18cba
Sujith Christopher <chrisbon95@gmail.com>
2023-03-29 17:27:23 +0530

    rs example

--- example_programs/realsense.py
+++ example_programs/realsense.py
@@ -38,12 +38,19 @@ try:
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
+        
         if not depth_frame or not color_frame:
             continue
 
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
+
+
+        # get real world coordinates
+
+
+        print(depth_image)
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
